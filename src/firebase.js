2312b712149fc219bd601092fb0ac7a7e3facf64
Sathishkumar Murugesan <65018309+Satiz-M@users.noreply.github.com>
2020-09-30 15:59:07 -0400

    firebase initilization

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,18 @@
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB89DTA89jfwO87Zp9nSyv853ip61AASA0",
-//   authDomain: "instagram1-clone.firebaseapp.com",
-//   databaseURL: "https://instagram1-clone.firebaseio.com",
-//   projectId: "instagram1-clone",
-//   storageBucket: "instagram1-clone.appspot.com",
-//   messagingSenderId: "622932907136",
-//   appId: "1:622932907136:web:b2bcb37f3e0a076c844265",
-//   measurementId: "G-3ZQ59HBGFH"
-// };
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyB89DTA89jfwO87Zp9nSyv853ip61AASA0",
+  authDomain: "instagram1-clone.firebaseapp.com",
+  databaseURL: "https://instagram1-clone.firebaseio.com",
+  projectId: "instagram1-clone",
+  storageBucket: "instagram1-clone.appspot.com",
+  messagingSenderId: "622932907136",
+  appId: "1:622932907136:web:b2bcb37f3e0a076c844265",
+  measurementId: "G-3ZQ59HBGFH"
+});
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+export default { db, auth, storage };
